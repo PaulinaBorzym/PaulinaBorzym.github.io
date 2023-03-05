eff4c059f73f6847b220afeb23d7c01173164bd7
Paulina Borym <paulina.borzym@op.pl>
2023-03-05 15:21:17 +0100

    return to previous value of tasksContainer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     var apiRoot = 'https://task.paulinaborzym1.repl.co/v1/tasks';
     var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-    var tasksContainer = $('[datatable__tasks-container]');
+    var tasksContainer = $('[data-tasks-container]');
   
     // init
     getAllTasks();
